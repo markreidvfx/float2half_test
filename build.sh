30bdfae794169dfdeffef2ac1e91cb12cc7178af
Mark Reid <mindmark@gmail.com>
2023-04-25 19:11:06 -0700

    auto build for arm

--- build.sh
+++ build.sh
@@ -1,25 +1,33 @@
+#!/bin/bash
 set -e
+machinestr=$(uname -m)
+echo $machinestr
 
-# x86
-gcc -mf16c -O3 float2half.c -o float2half
-gcc -mf16c -O3 half2float.c -o half2float
-./float2half
-./half2float
+if [ "$machinestr" = 'aarch64' ]; then
+# arm raspberry pi 64
+gcc -O3 float2half.c -DUSE_ARM -o float2half_aarch64
+gcc -O3 half2float.c -DUSE_ARM -o half2float_aarch64
+./float2half_aarch64
+./half2float_aarch64
 
-# # arm raspberry pi 32
-# gcc -mfpu=neon-fp-armv8 -mfloat-abi=hard -mfp16-format=ieee -O3 float2half.c -DUSE_ARM  -o float2half_arm
-# gcc -mfpu=neon-fp-armv8 -mfloat-abi=hard -mfp16-format=ieee -O3 half2float.c -DUSE_ARM  -o half2float_arm
-# ./float2half_arm
-# ./half2float_arm
-
-# # arm raspberry pi 64
-# gcc -O3 float2half.c -DUSE_ARM  -o float2half_aarch64
-# gcc-O3 half2float.c -DUSE_ARM  -o half2float_aarch64
-# ./float2half_aarch64
-# ./half2float_aarch64
-
-# # cross linux aarch64 qemu
+# ross linux aarch64 qemu
 # aarch64-linux-gnu-gcc -O3 float2half.c -DUSE_ARM -o float2half_aarch64
 # aarch64-linux-gnu-gcc -O3 half2float.c -DUSE_ARM -o half2float_aarch64
 # qemu-aarch64 -L /usr/aarch64-linux-gnu ./float2half_aarch64
 # qemu-aarch64 -L /usr/aarch64-linux-gnu ./half2float_aarch64
+
+elif  [ "$machinestr" = 'armv7l' ]; then
+# arm raspberry pi 32
+gcc -O3 -mfpu=neon-fp-armv8 -mfloat-abi=hard -mfp16-format=ieee float2half.c -DUSE_ARM  -o float2half_arm
+gcc -O3 -mfpu=neon-fp-armv8 -mfloat-abi=hard -mfp16-format=ieee half2float.c -DUSE_ARM  -o half2float_arm
+./float2half_arm
+./half2float_arm
+
+else
+# x86
+gcc -O3 -mf16c float2half.c -o float2half
+gcc -O3 -mf16c half2float.c -o half2float
+./float2half
+./half2float
+
+fi

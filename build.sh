ba387e98c43ab9ccc92be90af01d8b32a46d782f
Mark Reid <mindmark@gmail.com>
2023-06-11 00:58:25 -0700

    update build.sh

--- build.sh
+++ build.sh
@@ -10,19 +10,19 @@ $CC --version
 if [ "$machinestr" = 'aarch64' ]; then
 # arm raspberry pi 64
 $CC -O3 -DUSE_ARM -c hardware/hardware.c
-$CC -O3 -c table/table.c
-$CC -O3 -c table_round/table_round.c
-$CC -O3 -c no_table/no_table.c
-$CC -O3 -c cpython/cpython.c
-$CC -O3 -c numpy/numpy.c
-$CC -O3 -c imath/imath.c
-$CC -O3 -c tursa/tursa.c
-$CC -O3 -c ryg/ryg.c
-$CC -O3 -c maratyszcza/maratyszcza.c
-$CC -O3 -c maratyszcza_nanfix/maratyszcza_nanfix.c
+$CC -O3 -c src/table/table.c
+$CC -O3 -c src/table_round/table_round.c
+$CC -O3 -c src/no_table/no_table.c
+$CC -O3 -c src/cpython/cpython.c
+$CC -O3 -c src/numpy/numpy.c
+$CC -O3 -c src/imath/imath.c
+$CC -O3 -c src/tursa/tursa.c
+$CC -O3 -c src/ryg/ryg.c
+$CC -O3 -c src/maratyszcza/maratyszcza.c
+$CC -O3 -c src/maratyszcza_nanfix/maratyszcza_nanfix.c
 
-$CC -O3 float2half.c hardware.o table.o table_round.o no_table.o imath.o cpython.o numpy.o tursa.o ryg.o maratyszcza.o maratyszcza_nanfix.o -o float2half_aarch64
-$CC -O3 half2float.c hardware.o table.o table_round.o no_table.o imath.o cpython.o numpy.o tursa.o ryg.o maratyszcza.o maratyszcza_nanfix.o -o half2float_aarch64
+$CC -O3 src/float2half.c hardware.o table.o table_round.o no_table.o imath.o cpython.o numpy.o tursa.o ryg.o maratyszcza.o maratyszcza_nanfix.o -o float2half_aarch64
+$CC -O3 src/half2float.c hardware.o table.o table_round.o no_table.o imath.o cpython.o numpy.o tursa.o ryg.o maratyszcza.o maratyszcza_nanfix.o -o half2float_aarch64
 
 ./float2half_aarch64
 ./half2float_aarch64
@@ -38,44 +38,44 @@ elif  [ "$machinestr" = 'armv7l' ]; then
 # arm raspberry pi 32
 
 $CC -O3 -DUSE_ARM -mfpu=neon-fp-armv8 -mfloat-abi=hard -mfp16-format=ieee -c hardware/hardware.c
-$CC -O3 -c table/table.c
-$CC -O3 -c table_round/table_round.c
-$CC -O3 -c no_table/no_table.c
-$CC -O3 -c cpython/cpython.c
-$CC -O3 -c numpy/numpy.c
-$CC -O3 -c imath/imath.c
-$CC -O3 -c tursa/tursa.c
-$CC -O3 -c ryg/ryg.c
-$CC -O3 -c maratyszcza/maratyszcza.c
-$CC -O3 -c maratyszcza_nanfix/maratyszcza_nanfix.c
+$CC -O3 -c src/table/table.c
+$CC -O3 -c src/table_round/table_round.c
+$CC -O3 -c src/no_table/no_table.c
+$CC -O3 -c src/cpython/cpython.c
+$CC -O3 -c src/numpy/numpy.c
+$CC -O3 -c src/imath/imath.c
+$CC -O3 -c src/tursa/tursa.c
+$CC -O3 -c src/ryg/ryg.c
+$CC -O3 -c src/maratyszcza/maratyszcza.c
+$CC -O3 -c src/maratyszcza_nanfix/maratyszcza_nanfix.c
 
-$CC -O3 float2half.c hardware.o table.o table_round.o no_table.o imath.o cpython.o numpy.o tursa.o ryg.o maratyszcza.o maratyszcza_nanfix.o -o float2half_arm
-$CC -O3 half2float.c hardware.o table.o table_round.o no_table.o imath.o cpython.o numpy.o tursa.o ryg.o maratyszcza.o maratyszcza_nanfix.o -o half2float_arm
+$CC -O3 src/float2half.c hardware.o table.o table_round.o no_table.o imath.o cpython.o numpy.o tursa.o ryg.o maratyszcza.o maratyszcza_nanfix.o -o float2half_arm
+$CC -O3 src/half2float.c hardware.o table.o table_round.o no_table.o imath.o cpython.o numpy.o tursa.o ryg.o maratyszcza.o maratyszcza_nanfix.o -o half2float_arm
 
 ./float2half_arm
 ./half2float_arm
 
 else
 # x86
-$CC -O3 -mf16c -c hardware/hardware.c
-$CC -O3 -mtune=generic -c table/table.c
-$CC -O3 -mtune=generic -c table_round/table_round.c
-$CC -O3 -mtune=generic -c no_table/no_table.c
-$CC -O3 -mtune=generic -c cpython/cpython.c
-$CC -O3 -mtune=generic -c numpy/numpy.c
-$CC -O3 -mtune=generic -c imath/imath.c
-$CC -O3 -mtune=generic -c tursa/tursa.c
-$CC -O3 -mtune=generic -c ryg/ryg.c
-$CC -O3 -mtune=generic -c maratyszcza/maratyszcza.c
-$CC -O3 -mtune=generic -c maratyszcza_nanfix/maratyszcza_nanfix.c
-$CC -O3 -mtune=generic -msse2 -mno-sse3 -mno-sse4 -mno-sse4.2 -mno-avx -mno-avx2 -c maratyszcza_sse2/maratyszcza_sse2.c
-$CC -O3 -c x86_cpu_info.c
+$CC -O3 -mf16c -c src/hardware/hardware.c
+$CC -O3 -mtune=generic -c src/table/table.c
+$CC -O3 -mtune=generic -c src/table_round/table_round.c
+$CC -O3 -mtune=generic -c src/no_table/no_table.c
+$CC -O3 -mtune=generic -c src/cpython/cpython.c
+$CC -O3 -mtune=generic -c src/numpy/numpy.c
+$CC -O3 -mtune=generic -c src/imath/imath.c
+$CC -O3 -mtune=generic -c src/tursa/tursa.c
+$CC -O3 -mtune=generic -c src/ryg/ryg.c
+$CC -O3 -mtune=generic -c src/maratyszcza/maratyszcza.c
+$CC -O3 -mtune=generic -c src/maratyszcza_nanfix/maratyszcza_nanfix.c
+$CC -O3 -mtune=generic -msse2 -mno-sse3 -mno-sse4 -mno-sse4.2 -mno-avx -mno-avx2 -c src/maratyszcza_sse2/maratyszcza_sse2.c
+$CC -O3 -c src/x86_cpu_info.c
 #debug sse2
 # $CC -O3 -mtune=generic -msse2 -mf16c  -c maratyszcza_sse2/maratyszcza_sse2.c
 
 
-$CC -O3 float2half.c x86_cpu_info.o hardware.o table.o table_round.o no_table.o imath.o cpython.o numpy.o tursa.o ryg.o maratyszcza.o maratyszcza_nanfix.o maratyszcza_sse2.o -o float2half
-$CC -O3 half2float.c x86_cpu_info.o hardware.o table.o table_round.o no_table.o imath.o cpython.o numpy.o tursa.o ryg.o maratyszcza.o maratyszcza_nanfix.o maratyszcza_sse2.o -o half2float
+$CC -O3 src/float2half.c x86_cpu_info.o hardware.o table.o table_round.o no_table.o imath.o cpython.o numpy.o tursa.o ryg.o maratyszcza.o maratyszcza_nanfix.o maratyszcza_sse2.o -o float2half
+$CC -O3 src/half2float.c x86_cpu_info.o hardware.o table.o table_round.o no_table.o imath.o cpython.o numpy.o tursa.o ryg.o maratyszcza.o maratyszcza_nanfix.o maratyszcza_sse2.o -o half2float
 
 ./float2half
 ./half2float
